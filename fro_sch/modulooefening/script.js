88fc07509e995a6b12557eadd8cf1d627a710135
Pariek6 <patrickfrumau@hotmail.com>
2017-10-06 11:11:49 +0200

    Add files via upload

---
+++ fro_sch/modulooefening/script.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 100; i++) {
+
+  if (i % 5 == 0 && i % 3 == 0) { console.log('MediaCollege'); }
+
+  else if (i % 3 == 0) {console.log('Media'); }
+
+  else if (i % 5 == 0) { console.log('College'); }
+
+
+  else { console.log(i); }
+}
