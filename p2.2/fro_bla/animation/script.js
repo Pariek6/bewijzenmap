a7d05a2339ff4987052982eda46a976e32f59fe1
Pariek6 <patrickfrumau@hotmail.com>
2018-01-19 10:18:18 +0100

    Update script.js

--- p2.2/fro_bla/animation/script.js
+++ p2.2/fro_bla/animation/script.js
@@ -1,34 +1,10 @@
 const button = document.getElementById('button');
-let square_one = document.querySelector('.square-one')
-let square_two = document.querySelector('.square-two')
-let square_three = document.querySelector('.square-three')
+let vlak = document.querySelectorAll('.vlakje')
 
-const switch_state =()=>{
-    square_one.classList.toggle('square-one--extend');
-    square_two.classList.toggle('square-two--extend');
-    square_three.classList.toggle('square-three--extend');
+const schuif = () => {
+  for(i = 0; i < vlak.length; i++) {
+      vlak[i].classList.toggle('vlakje--schuif-uit');
+  }
 };
 
-const switch_state_one =()=>{
-    square_one.classList.toggle('square-one--extend');
-    square_two.classList.toggle('square-two--extend');
-    square_three.classList.toggle('square-three--extend');
-};
-
-const switch_state_two =()=>{
-    square_one.classList.toggle('square-one--extend');
-    square_two.classList.toggle('square-two--extend');
-    square_three.classList.toggle('square-three--extend');
-};
-
-const switch_state_three =()=>{
-    square_one.classList.toggle('square-one--extend');
-    square_two.classList.toggle('square-two--extend');
-    square_three.classList.toggle('square-three--extend');
-};
-
-
-square_one.addEventListener('click', switch_state_one);
-square_two.addEventListener('click', switch_state_two);
-square_three.addEventListener('click', switch_state_three);
-button.addEventListener('click', switch_state);
+button.addEventListener('click', schuif)
